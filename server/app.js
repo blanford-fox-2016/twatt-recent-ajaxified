c2a1fc7d805ef8503930462d69c670adc856e722
Septian A Tama <sfirst.tama@gmail.com>
2016-10-31 17:52:00 +0700

    fixing error from render error page

--- server/app.js
+++ server/app.js
@@ -55,7 +55,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
-  res.render('error', {
+  res.send({
     message: err.message,
     error: {}
   });
