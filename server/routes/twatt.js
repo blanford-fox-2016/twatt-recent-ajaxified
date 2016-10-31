b7fc2ceeae9594086503d41d8aa7fad7b5ce6f3e
Septian A Tama <sfirst.tama@gmail.com>
2016-10-31 16:07:32 +0700

    add route for new tweet and search

--- server/routes/twatt.js
+++ server/routes/twatt.js
@@ -6,7 +6,9 @@ let oauth = require('oauth');
 
 let twitter = require('../controllers/TwitterController.js')
 
-router.get('/my_timeline', twitter.getUserTimeline)
-router.get('/home_timeline', twitter.getHomeTimeline)
+router.get('/my_timeline', twitter.getUserTimeline);
+router.get('/home_timeline', twitter.getHomeTimeline);
+router.get('/search', twitter.searchTweet);
+router.post('/tweet', twitter.postTweet);
 
 module.exports = router;
