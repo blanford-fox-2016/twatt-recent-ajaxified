50ef8ea2103ec62c44a493d6c9a7032917ab4277
Alexander Hendrawan tjahja & Ken Duigraha Putra <pair+alexanderHT.kenduigraha@hacktiv8.com>
2016-10-31 10:52:15 +0700

    installed cors, success show data json to client

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var api = require('./routes/api');
 
 var app = express();
+
+// cors
+app.use(cors());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
