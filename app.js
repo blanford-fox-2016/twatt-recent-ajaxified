a4b4471b825396e0af089b99cf842ea765e64570
Alexander Hendrawan tjahja & Ken Duigraha Putra <pair+alexanderHT.kenduigraha@hacktiv8.com>
2016-10-31 10:48:45 +0700

    checkpoint: added client/index.html

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var timeline = require('./routes/timeline');
 var api = require('./routes/api');
 
 var app = express();
@@ -19,9 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 app.use('/api', api);
-app.use('/timeline', timeline);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
