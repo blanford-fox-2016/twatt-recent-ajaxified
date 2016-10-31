194f13acb7b605031fc22752df054e5f08b63890
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-10-31 18:44:16 +0700

    fix bug ajax

--- server/config/config.js
+++ server/config/config.js
@@ -5,10 +5,18 @@ module.exports = {
   //   'accessToken': '62780570-F27D3xjdziTf4eNGODZseM14HgaXFiWBccvlsu18Z',
   //   'accessTokenSecret': 'R0Kl1qg4I4SaGpwh37K3MBnb0L3lIIr6VIjMQZzuPiffN'
   // }
+
+  // 'twitterAuth': {
+  //   'consumerKey': 'eHaVkJXsyQ9s3h7oOCsbauoVj',
+  //   'consumerSecret': 'yppcmGV45nScBpye1rP23PUvr4OwCRGhqJdamTPsXvdWGm7NoW',
+  //   'accessToken': '452037695-QWsiuv6kHxzHAdrjJ9tNjdo5uReJz4sALO9FtMvA',
+  //   'accessTokenSecret': 'Lkmy4aShzXrrrNeZ5Fbiy7uezLiIdKJHWE888o5lD22TE'
+  // }
+
   'twitterAuth': {
-    'consumerKey': 'eHaVkJXsyQ9s3h7oOCsbauoVj',
-    'consumerSecret': 'yppcmGV45nScBpye1rP23PUvr4OwCRGhqJdamTPsXvdWGm7NoW',
-    'accessToken': '452037695-QWsiuv6kHxzHAdrjJ9tNjdo5uReJz4sALO9FtMvA',
-    'accessTokenSecret': 'Lkmy4aShzXrrrNeZ5Fbiy7uezLiIdKJHWE888o5lD22TE'
+    'consumerKey': 'PUVrm0PcRGICBYJkx2Vgn2UqI',
+    'consumerSecret': 'PBOIc4GWc8SA6wd836Iwt4xbpjvVUVWVAG0ldXTRKQQf00mVMF',
+    'accessToken': '780647183234195456-MuN7SDrLFjxZcVMR0xGnAjGl5qiBDPw',
+    'accessTokenSecret': 'An6V8gmmFmOJ8vhWsCFflz1lBPqlRTcFtrXgu34bCuTZl'
   }
 }
