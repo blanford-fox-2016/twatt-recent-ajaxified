79388b6165a077860b32d5f368d950af8b9fb8fc
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-03 15:18:16 +0700

    refactor code yang tidak terpakai

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  // 'twitterAuth': {
-  //   'consumerKey': 'KtMpYIznZhJeuvqLUHBJHJvi1',
-  //   'consumerSecret': 'zNnfNoFk0MZqa2fPnOFLy6RIcG7XZV1Lrtp0hOAvM0D3uh79yi',
-  //   'accessToken': '62780570-F27D3xjdziTf4eNGODZseM14HgaXFiWBccvlsu18Z',
-  //   'accessTokenSecret': 'R0Kl1qg4I4SaGpwh37K3MBnb0L3lIIr6VIjMQZzuPiffN'
-  // }
+  'twitterAuth': {
+    'consumerKey': 'KtMpYIznZhJeuvqLUHBJHJvi1',
+    'consumerSecret': 'zNnfNoFk0MZqa2fPnOFLy6RIcG7XZV1Lrtp0hOAvM0D3uh79yi',
+    'accessToken': '62780570-F27D3xjdziTf4eNGODZseM14HgaXFiWBccvlsu18Z',
+    'accessTokenSecret': 'R0Kl1qg4I4SaGpwh37K3MBnb0L3lIIr6VIjMQZzuPiffN'
+  }
 
   // 'twitterAuth': {
   //   'consumerKey': 'eHaVkJXsyQ9s3h7oOCsbauoVj',
@@ -13,10 +13,10 @@ module.exports = {
   //   'accessTokenSecret': 'Lkmy4aShzXrrrNeZ5Fbiy7uezLiIdKJHWE888o5lD22TE'
   // }
 
-  'twitterAuth': {
-    'consumerKey': 'PUVrm0PcRGICBYJkx2Vgn2UqI',
-    'consumerSecret': 'PBOIc4GWc8SA6wd836Iwt4xbpjvVUVWVAG0ldXTRKQQf00mVMF',
-    'accessToken': '780647183234195456-MuN7SDrLFjxZcVMR0xGnAjGl5qiBDPw',
-    'accessTokenSecret': 'An6V8gmmFmOJ8vhWsCFflz1lBPqlRTcFtrXgu34bCuTZl'
-  }
+  // 'twitterAuth': {
+  //   'consumerKey': 'PUVrm0PcRGICBYJkx2Vgn2UqI',
+  //   'consumerSecret': 'PBOIc4GWc8SA6wd836Iwt4xbpjvVUVWVAG0ldXTRKQQf00mVMF',
+  //   'accessToken': '780647183234195456-MuN7SDrLFjxZcVMR0xGnAjGl5qiBDPw',
+  //   'accessTokenSecret': 'An6V8gmmFmOJ8vhWsCFflz1lBPqlRTcFtrXgu34bCuTZl'
+  // }
 }
