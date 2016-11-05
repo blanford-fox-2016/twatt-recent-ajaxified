64f0a3f75b2da9feea2cbf6300399af161482ffc
Ken Duigraha utra <kenduigraha@yahoo.com>
2016-11-05 23:03:29 +0700

    config.js template

---
+++ helper/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  request_token: 'https://api.twitter.com/oauth/request_token',
+  access_token: 'https://api.twitter.com/oauth/access_token',
+  consumer_key: '',
+  application_secret: '',
+  access_token : '',
+  access_token_secret : '',
+  ver: '1.0A',
+  null: null,
+  algo: 'HMAC-SHA1',
+
+}
